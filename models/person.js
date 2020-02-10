2ed04871e5ceb52343811ef786b192bcdace8b30
Mikxdi <mikko.lehtinen1996@gmail.com>
2020-02-10 19:07:34 +0200

    viimeistelya

--- models/person.js
+++ models/person.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 mongoose.set('useFindAndModify', false)
-const uri =process.env.MONGODB_URI
+const url = String(process.env.MONGODB_URI)
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(result => {
     console.log('connected to MongoDB');
   })
